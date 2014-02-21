70a2986b838c4147eeb9abed0f2445cb1608e156
Fabian Hirschmann <fabian@hirschm.net>
2014-02-21 02:27:49 +0100

    numpy version of rdp

--- rdp.py
+++ rdp.py
@@ -6,23 +6,24 @@ def pldist(x0, x1, x2):
                      np.linalg.norm(x2 - x1))
 
 
-def rdp(points, epsilon=0, dist=pldist):
-    if epsilon <= 0:
-        return points
-
-    if len(points) < 3:
-        return points
+def rdp(M, epsilon=0, dist=pldist):
+    if M.shape[0] < 3:
+        return M
 
     dmax = 0.0
     index = -1
-    for i in xrange(1, len(points)):
-        d = pldist(np.array(points[i]), np.array(points[0]), np.array(points[-1]))
+
+    for i in xrange(1, M.shape[0]):
+        d = dist(M[i], M[0], M[-1])
+
         if d > dmax:
             index = i
             dmax = d
+
     if dmax > epsilon:
-        r1 = rdp(points[:index + 1], epsilon)
-        r2 = rdp(points[index:], epsilon)
-        return r1[:-1] + r2
+        r1 = rdp(M[:index + 1], epsilon)
+        r2 = rdp(M[index:], epsilon)
+
+        return np.vstack((r1[:-1], r2))
     else:
-        return [points[0], points[-1]]
+        return np.vstack((M[0], M[-1]))
