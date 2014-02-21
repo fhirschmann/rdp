fc36c0e29e527925e23f92a2a3ad5cf6a8e14a5c
Fabian Hirschmann <fabian@hirschm.net>
2014-02-21 22:48:13 +0100

    documented code

--- rdp/__init__.py
+++ rdp/__init__.py
@@ -1,12 +1,43 @@
+"""
+rdp
+~~~
+
+Pure Python implementation of the Ramer-Douglas-Peucker algorithm.
+
+:copyright: (c) 2014 Fabian Hirschmann <fabian@hirschm.net>
+:license: MIT, see LICENSE.txt for more details.
+
+"""
 import numpy as np
 
 
 def pldist(x0, x1, x2):
+    """
+    Calculates the distance from the point ``x1`` to the line given
+    by the points ``x1`` and ``x2``.
+
+    :param x0: a point
+    :type x0: a 2x1 numpy array
+    :param x1: a point of the line
+    :type x1: 2x1 numpy array
+    :param x2: another point of the line
+    :type x2: 2x1 numpy array
+    """
     return np.divide(np.linalg.norm(np.linalg.det([x2 - x1, x1 - x0])),
                      np.linalg.norm(x2 - x1))
 
 
 def _rdp(M, epsilon, dist):
+    """
+    Simplifies a given array of points.
+
+    :param M: an array
+    :type M: Nx2 numpy array
+    :param epsilon: epsilon in the rdp algorithm
+    :type epsilon: float
+    :param dist: distance function
+    :type dist: function with signature ``f(x1, x2, x3)``
+    """
     dmax = 0.0
     index = -1
 
@@ -27,10 +58,30 @@ def _rdp(M, epsilon, dist):
 
 
 def _rdp_nn(seq, epsilon, dist):
+    """
+    Simplifies a given array of points.
+
+    :param seq: a series of points
+    :type seq: sequence of 2-tuples
+    :param epsilon: epsilon in the rdp algorithm
+    :type epsilon: float
+    :param dist: distance function
+    :type dist: function with signature ``f(x1, x2, x3)``
+    """
     return rdp(np.array(seq), epsilon, dist).tolist()
 
 
 def rdp(M, epsilon=0, dist=pldist):
+    """
+    Simplifies a given array of points.
+
+    :param M: a series of points
+    :type M: either a Nx2 numpy array or sequence of 2-tuples
+    :param epsilon: epsilon in the rdp algorithm
+    :type epsilon: float
+    :param dist: distance function
+    :type dist: function with signature ``f(x1, x2, x3)``
+    """
     if "numpy" in str(type(M)):
         return _rdp(M, epsilon, dist)
     return _rdp_nn(M, epsilon, dist)
