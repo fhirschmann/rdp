ce38daa1ec5fadb9739ce8f37acd938fb7061daf
Fabian Hirschmann <fabian@hirschmann.email>
2016-10-08 02:10:44 +0200

    rtd mock

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,11 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+import mock
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
